265183b1c3d0fdc4476ed3da2efdc94632dce6ca
Jafar Hussein <Jafarhussein6@gmail.com>
2023-10-09 20:53:26 +0200

    Fixade kommentar

--- WebApi/src/main/java/com/example/interfaces/MovieRepository.java
+++ WebApi/src/main/java/com/example/interfaces/MovieRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRepository extends JpaRepository<MovieInfo, Long> {
+public interface MovieRepository extends JpaRepository<MovieInfo, Long> { // JpaRepository är en interface som innehåller metoder för att hämta data från databasen
+    // JpaRepository<MovieInfo, Long> säger att vi vill hämta data från databasen av typen MovieInfo och att id:n är av typen Long
 
 }
