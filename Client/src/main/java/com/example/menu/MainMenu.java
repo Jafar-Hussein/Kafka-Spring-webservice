634c800c5ab286dbfb6d589adb8b9274b1405457
Jafar Hussein <Jafarhussein6@gmail.com>
2023-10-09 21:02:55 +0200

    Fixade kommentarer

--- Client/src/main/java/com/example/menu/MainMenu.java
+++ Client/src/main/java/com/example/menu/MainMenu.java
@@ -62,6 +62,7 @@ public void runMenu() {
 
 
     private static void addMovie() { // metod som skapar en ny film
+    // skapar en ny film med hjälp av input från användaren
         System.out.println("Enter id");
         long id = inputHandler.getLongInput();
         inputHandler.getStringInput(); //Konsumerar new line character
@@ -71,23 +72,27 @@ public void runMenu() {
         String genre = inputHandler.getStringInput();
         System.out.print("Enter Movie Release Date: ");
         String releaseDate = inputHandler.getStringInput();
-
+        // Skapar en JSON-objekt
         JSONObject myObj = new JSONObject();
+        // Lägger till värden till JSON-objektet
         myObj.put("id", id);
         myObj.put("movieTitle", title);
         myObj.put("movieGenre", genre);
         myObj.put("movieReleaseDate", releaseDate);
-
+        // Skickar JSON-objektet till WebAPI
         ClientConsumer.sendToWebAPI(myObj);
     }
 
     private void menyOptions(){
+        //metod som skriver ut meny
         String[] options = {"Main Menu:","1. Add a movie", "2. View movies", "3. Exit"};
+        // loopar igenom arrayen och skriver ut varje element genom att använda for each loop
         for (String option : options) {
             System.out.println(option);
         }
     }
     private int getUserChoice() {
+    //metod som tar in input från användaren
         System.out.print("Select an option:\n ");
         return inputHandler.getIntInput();
     }
