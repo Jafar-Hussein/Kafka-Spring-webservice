1b343bf93a7c9e521e2e2a7f9a191fa73829a8bb
Jafar Hussein <Jafarhussein6@gmail.com>
2023-10-09 21:06:44 +0200

    Fixade mer kommentarer

--- Client/src/main/java/com/example/menu/MainMenu.java
+++ Client/src/main/java/com/example/menu/MainMenu.java
@@ -22,25 +22,33 @@ public void runMenu() {
         //anropar meny med olika val och switch case
         while (true) {
             try {
+                //skriver ut meny
                 menyOptions();
+                //tar in input från användaren
                 int choice = getUserChoice();
+                //switch case som kollar vilket val användaren har gjort och sedan anropar olika metoder för olika funktioner
                 switch (choice) {
+                    //anropar metoden addMovie för att lägga till en ny film
                     case 1:
                         inputHandler.getStringInput();
                         addMovie();
                         break;
+                        //anropar metoden viewMovies för att visa alla filmer
                     case 2:
                         inputHandler.getStringInput(); // konsumerar new line character
                         viewMovies();
                         break;
+                        //anropar metoden exit för att avsluta applikationen
                     case 3:
                         exit();
                         break;
+                        //om användaren inte har valt något av alternativen så skrivs ett felmeddelande ut
                     default:
                         System.out.println("Invalid input. Please enter a valid option.");
                         break;
 
                 }
+                //om input inte är en nummer så kastas exception och användaren får försöka igen
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input. Please enter a valid option.");
                 inputHandler.getStringInput(); // Consume the newline character
@@ -50,12 +58,14 @@ public void runMenu() {
     }
 
     private void exit() {
+    //metod som avslutar applikationen genom att anropa System.exit(0)
         System.out.println("Exiting the application.");
         System.exit(0);
     }
 
 
     private static void viewMovies() {
+    // metod som hämtar data från kafka och skriver ut den i klient applikationen
         ClientConsumer.getDataFromKafka("movieListTopic");
     }
 
